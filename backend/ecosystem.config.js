a25e2082a97b880a91d4a409c1788afa1cc3b063
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-07-05 23:15:16 +0300

    2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -39,7 +39,6 @@ module.exports = {
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': `mkdir ${DEPLOY_PATH} && cp -Rf ./backend/* ${DEPLOY_PATH} && cp ${TEMP_PATH}/.env ${DEPLOY_PATH} && rm -rf ${TEMP_PATH} && cd ${DEPLOY_PATH} && npm i && npm run build && pm2 kill && pm2 start ecosystem.config.js && pm2 save`,
-      // 'post-deploy': 'npm i && npm run build && pm2 kill && pm2 start ecosystem.config.js && pm2 save',
     },
   },
 };
