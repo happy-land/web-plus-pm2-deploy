731b0d69b13b682fdd1d3aaa94c7017ee0cedcf7
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-07-07 20:48:29 +0300

    app.ts package.json updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -14,7 +14,20 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors());
+app.use(cors());
+
+app.use((req: Request, res: Response, next) => {
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://rus-frontend.nomoreparties.sbs');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
